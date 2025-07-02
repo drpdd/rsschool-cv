208d1a5f3a42bd5fad77f350b5dc40129e950c00
drpdd <dropdeaddropdead1488@gmail.com>
2025-07-02 18:49:11 +0300

    fix: deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
 	plugins: [
@@ -12,12 +12,6 @@ export default defineConfig({
 	],
 	base: '/rsschool-cv/',
 	build: {
-		outDir: 'dist',
-		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				main: './index.html'
-			}
-		}
+		outDir: 'dist'
 	}
 });
